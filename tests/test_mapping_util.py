5f8fce28e2b445cc92539b11aca7e774b92139fe
Vince Luk <vinceluk@squaretrade.com>
2019-10-16 12:34:22 -0700

    updated mapping_util to support list and tested save for ModelWrapper
    * get_player_info_by_it now supports passing a list
    * added test to train model, save, then load it back up in ModelWrapper

--- tests/test_mapping_util.py
+++ tests/test_mapping_util.py
@@ -18,10 +18,24 @@ def test_get_player_info_by_id(shared_datadir):
     :return:
     """
     Mapper.PLAYERS_FILE = f'{shared_datadir}/players.csv'
-    info = Mapper.get_player_info_by_id(player_id = "102778")
+    info = Mapper.get_player_info_by_id(player_ids="102778")
     assert len(info) == 1, "returned number of rows mismatched"
     assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
 
+def test_get_player_info_by_id_list(shared_datadir):
+    """
+    test lookup player by player_id
+    :param shared_datadir:
+    :return:
+    """
+    Mapper.PLAYERS_FILE = f'{shared_datadir}/players.csv'
+    info = Mapper.get_player_info_by_id(player_ids=["102778"])
+    assert len(info) == 1, "returned number of rows mismatched"
+    assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
+
+    info = Mapper.get_player_info_by_id(player_ids=["102778", "106361"])
+    assert len(info) == 2, "returned number of rows mismatched"
+    assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
 
 def test_get_player_info_by_ohe_col(shared_datadir):
     """
@@ -30,11 +44,26 @@ def test_get_player_info_by_ohe_col(shared_datadir):
     :return:
     """
     Mapper.PLAYERS_FILE = f'{shared_datadir}/players.csv'
-    info = Mapper.get_player_info_by_id(ohe_column = "p1_102778")
+    info = Mapper.get_player_info_by_id(ohe_columns="p1_102778")
     assert len(info) == 1, "returned number of rows mismatched"
     assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
 
-    info = Mapper.get_player_info_by_id(ohe_column = "p2_102778")
+    info = Mapper.get_player_info_by_id(ohe_columns="p2_102778")
     assert len(info) == 1, "returned number of rows mismatched"
     assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
 
+def test_get_player_info_by_ohe_col_list(shared_datadir):
+    """
+    test lookup of a person from players.csv using ohe_column parameter
+    :param shared_datadir:
+    :return:
+    """
+    Mapper.PLAYERS_FILE = f'{shared_datadir}/players.csv'
+    info = Mapper.get_player_info_by_id(ohe_columns=["p1_102778"])
+    assert len(info) == 1, "returned number of rows mismatched"
+    assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
+
+
+    info = Mapper.get_player_info_by_id(ohe_columns=["p2_102778", "p1_106361"])
+    assert len(info) == 2, "returned number of rows mismatched"
+    assert info.name.values[0] == "abdul hamid makhkamov", "returned player name mismatch"
